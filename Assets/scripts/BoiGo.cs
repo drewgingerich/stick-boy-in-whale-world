dcc4924a8628291156824007c654de0754ea1387
Drew Gingerich <gingerich93@gmail.com>
2018-06-24 00:39:24 -0700

    Add character controller scripts.

---
+++ Assets/scripts/BoiGo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BoiGo : MonoBehaviour {
+
+	[Header("Oh shit!")]
+	public float acceleration = 40;
+	public float maxVelocity = 20;
+	public float friction = 2;
+
+	Rigidbody2D rb2d;
+	Vector2 inputDirection;
+	Vector2 deltaVelocity;
+
+	void Awake() {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		UpdateInputDirection();
+	}
+
+	void FixedUpdate() {
+		if (inputDirection != Vector2.zero)
+			ApplyInputForce();
+		ApplyFrictionForce();
+		rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxVelocity);
+	}
+
+	void UpdateInputDirection() {
+		inputDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+	}
+
+	void ApplyInputForce() {
+		Vector2 inputForce = inputDirection * acceleration;
+		rb2d.AddForce(inputForce);
+	}
+
+	void ApplyFrictionForce() {
+		if (rb2d.velocity.magnitude < friction * Time.fixedDeltaTime)
+			rb2d.velocity = Vector2.zero;
+		else
+			rb2d.AddForce(-rb2d.velocity.normalized * friction);
+	}
+}
