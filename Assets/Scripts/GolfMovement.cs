a569483d6b24d321fb8e2db4a724ba8dc71ca3d0
Matthew Conto <conto@nyu.edu>
2018-06-25 15:28:48 -0700

    Testing using a dragging system for movement

---
+++ Assets/Scripts/GolfMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfMovement : MonoBehaviour {
+	
+	Vector3 dbg_target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Touch touch = Input.GetTouch(0);
+		
+	}
+
+	/// <summary>
+	/// Routine for dragging a golf swing
+	/// </summary>
+	/// <returns></returns>
+	IEnumerator GolfSwingRoutine() {
+		yield return null;
+	}
+}
