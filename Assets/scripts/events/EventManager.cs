a1dd8eb05f6f8725d5f810d0617e50c7bc38d407
Drew Gingerich <gingerich93@gmail.com>
2018-06-25 19:21:17 -0700

    Add very basic minigame event system.

---
+++ Assets/scripts/events/EventManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+	public static EventManager instance;
+
+	[SerializeField] List<EventTracker> trackers;
+
+	EventTracker activeEventTracker;
+
+	void Awake() {
+		if (instance != null)
+			Debug.LogError("Mulitple event manager instances.");
+		instance = this;
+	}
+
+	public void FindNextEvent() {
+		while (activeEventTracker == null) {
+			int randIndex = Random.Range(0, trackers.Count);
+			if (trackers[randIndex].IsReady())
+				activeEventTracker = trackers[randIndex];
+		}
+		activeEventTracker.StartEvent();
+	}
+}
---
+++ Assets/scripts/events/EventTracker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventTracker : MonoBehaviour {
+
+	public UnityEvent OnStartEvent;
+
+	public void StartEvent() {
+		OnStartEvent.Invoke();
+	}
+
+	public bool IsReady() {
+		return true;
+	}
+
+	public void FinishEvent() {
+		EventManager.instance.FindNextEvent();
+	}
+}
