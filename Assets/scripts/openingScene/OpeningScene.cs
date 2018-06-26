4f7ed7e3e7b04234f548d4f92b9f9b26886a2378
Drew Gingerich <gingerich93@gmail.com>
2018-06-25 20:59:24 -0700

    Add opening scene and associated scripts

---
+++ Assets/scripts/openingScene/OpeningScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpeningScene : MonoBehaviour {
+
+	[SerializeField] List<Sprite> sprites;
+	[SerializeField] new SpriteRenderer renderer;
+	[SerializeField] new OpeningSceneCamera camera;
+
+	IEnumerator Start () {
+		for (int i = 0; i < sprites.Count; i++) {
+			renderer.sprite = sprites[i];
+			Vector2 cameraMove = new Vector2(Random.Range(-1, 2), Random.Range(-1, 2)); 
+			camera.Move(cameraMove);
+			yield return new WaitForSeconds(4);
+		}
+		SceneManager.LoadScene(1);
+	}
+}
