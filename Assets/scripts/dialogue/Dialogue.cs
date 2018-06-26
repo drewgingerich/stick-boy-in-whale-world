b425fa5b322eba8a7be0ef02059e27d736c71635
Drew Gingerich <gingerich93@gmail.com>
2018-06-25 18:43:01 -0700

    Add dialogue manager and dialogue scriptableobject.

---
+++ Assets/scripts/dialogue/Dialogue.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class Dialogue : ScriptableObject {
+
+	public string name;
+	public string[] lines;
+}
---
+++ Assets/scripts/dialogue/DialogueManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+	public static DialogueManager instance;
+
+	[SerializeField] Text dialogueText;
+	[SerializeField] Animator animator;
+
+	Queue<string> lines;
+	// Coroutine talkRoutine;
+
+	void Awake() {
+		if (instance != null)
+			Debug.LogError("Multiple WhaleTalk Instances.");
+		instance = this;
+		lines = new Queue<string>();
+	}
+
+	public void Talk(Dialogue dialogue) {
+		lines.Clear();
+		foreach (string line in dialogue.lines) {
+			lines.Enqueue(line);
+		}
+		DisplayNextLine();
+		// if (talkRoutine != null)
+		// 	StopCoroutine(talkRoutine);
+		// talkRoutine = StartCoroutine(TalkRoutine());
+	}
+
+	public void DisplayNextLine() {
+		if (lines.Count == 0)
+			EndDialogue();
+		dialogueText.text = lines.Dequeue();
+	}
+
+	void EndDialogue() {
+
+	}
+}
