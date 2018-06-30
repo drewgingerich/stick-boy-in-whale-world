7b1f2f71e59aaa9da413754adc3c284d9dffc1b5
Matthew Conto <conto@nyu.edu>
2018-06-29 20:28:56 -0700

    folder structure for tutorials, starting work on ending scene

---
+++ Assets/prefabs/GameEndingPhase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEndingPhase : MonoBehaviour {
+
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartEnding() {
+		//darken the whale
+		// start calling out to the player
+			// after x seconds, give a direct command
+		// open the blockade
+	}
+}
