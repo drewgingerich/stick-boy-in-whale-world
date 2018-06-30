818f7d938155fab64a6d3a2464310bbc4dcc3000
Matthew Conto <conto@nyu.edu>
2018-06-30 13:10:07 -0700

    Creating particle hit effect

---
+++ Assets/Scripts/Particles/PlaySparks.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySparks : MonoBehaviour {
+
+	public float lifespan = .5f;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds( lifespan);
+		Destroy(gameObject);
+	}
+
+}
