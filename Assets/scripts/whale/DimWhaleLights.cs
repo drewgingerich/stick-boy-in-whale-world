b6214de1b8c1f8fc6b0d568fc5d4a22704a6c2e4
Drew Gingerich <gingerich93@gmail.com>
2018-07-02 20:49:34 -0700

    Add script to dim whale sprite after whale's death

---
+++ Assets/scripts/whale/DimWhaleLights.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimWhaleLights : MonoBehaviour {
+
+	[SerializeField] SpriteRenderer whaleFloorSprite;
+	[SerializeField] SpriteRenderer whaleWallTopSprite;
+
+	Color dimColor = new Color(128, 125, 156);
+
+	public void DimLights() {
+		whaleFloorSprite.color = dimColor;
+		whaleWallTopSprite.color = dimColor;
+	}
+}
