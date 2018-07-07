2f6c4b2b26c633bd4f00cf1df688ec85546f0bca
Drew Gingerich <gingerich93@gmail.com>
2018-07-07 14:40:20 -0700

    Add scripts to start the endgame phase, and add to whale scene.

---
+++ Assets/scripts/events/TriggerEventOnEnter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+public class TriggerEventOnEnter : MonoBehaviour {
+
+	public UnityEvent OnEnterTrigger;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		OnEnterTrigger.Invoke();
+	}
+}
---
+++ Assets/scripts/events/WhaleEventFailMonitor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WhaleEventFailMonitor : MonoBehaviour {
+
+	public UnityEvent OnFailEvent;
+
+	[SerializeField] List<WhaleEvent> failableEvents;
+
+	void Start() {
+		foreach (WhaleEvent failableEvent in failableEvents) {
+			failableEvent.OnFail.AddListener(DetectEventFail);
+		}
+	}
+
+	void DetectEventFail() {
+		OnFailEvent.Invoke();
+	}
+}
