4fa6822330f337e0c88eb67c4bc2d6f67a3f957e
Drew Gingerich <gingerich93@gmail.com>
2018-07-07 16:06:02 -0700

    Rename EventDelay script to the more accurate name EventRepeater.

---
+++ Assets/scripts/events/EventRepeater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventRepeater : MonoBehaviour {
+
+	public UnityEvent OnRepeatEvent;
+
+	public float repeatDelay = 5f;
+
+	public void RepeatEvent() {
+		StartCoroutine(RepeatEventRoutine());
+	}
+
+	IEnumerator RepeatEventRoutine() {
+		yield return new WaitForSeconds(repeatDelay);
+		OnRepeatEvent.Invoke();
+	}
+}
