79ca176a47551ff6a384600d68a6bbec178bdb00
Drew Gingerich <gingerich93@gmail.com>
2018-07-14 15:57:48 -0700

    Separate throat debris into separate minigame. Add rising difficult to throat minigame.

---
+++ Assets/scripts/debris/ThroatDebris.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ThroatDebris : MonoBehaviour {
+
+	public UnityEvent OnSucceed;
+
+	[SerializeField] Debris debris;
+	[SerializeField] int startingDifficulty = 1;
+	[SerializeField] int difficultyMultiplier = 3;
+
+	int difficulty;
+
+	void Awake() {
+		difficulty = startingDifficulty;
+	}
+
+	void Start() {
+		debris.OnBreak += Succeed;
+		debris.gameObject.SetActive(false);
+	}
+
+	public void StartMinigame() {
+		debris.Spawn(difficulty * difficultyMultiplier);
+	}
+
+	void Succeed() {
+		OnSucceed.Invoke();
+	}
+
+	public void OnKillWhale() {
+		debris.OnBreak -= Succeed;
+	}
+}
