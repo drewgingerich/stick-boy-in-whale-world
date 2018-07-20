8858fb8d923c7aadab5cafa010282edf8e17e46c
Drew Gingerich <gingerich93@gmail.com>
2018-07-19 21:43:52 -0700

    Link animations to stickboi controller.

--- Assets/scripts/stickboi/StickboiController.cs
+++ Assets/scripts/stickboi/StickboiController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class StickboiController : MonoBehaviour {
 
+	[SerializeField] Animator anim;
+	[SerializeField] SpriteRenderer sprite;
 	[SerializeField] float speed = 10;
 
 	Rigidbody2D rb2d;
@@ -37,11 +39,15 @@ public class StickboiController : MonoBehaviour {
 			return;
 		Vector2 inputDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;		
 		rb2d.velocity = (Vector3)inputDirection * speed;
+		sprite.flipX = rb2d.velocity.x < 0 ? true : false;
+		bool moving = rb2d.velocity.magnitude > 0.1f ? true : false;
+		anim.SetBool("moving", moving);
 		if (Input.GetMouseButtonDown(0))
 			SwingStick();
 	}
 
 	void SwingStick() {
+		anim.SetTrigger("swing");
 		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		Vector2 direction = mousePosition - transform.position;
 		direction = direction.normalized;
